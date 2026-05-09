3bc4107178b02a0718293ec98a11a32854f95733
Lee Gyu Chan <hoo788@gmail.com>
2026-05-09 16:27:35 +0900

    initial commit

---
+++ src/test/java/com/eodegano/cocobackend/CocoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eodegano.cocobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocoBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
